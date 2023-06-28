5b37884b6b21c1760b4f9f9f5b1e5dd8dc7cd521
Александр <alteleg@yandex.ru>
2023-06-29 01:25:34 +0300

    Fixes

--- src/js/routes/index.js
+++ src/js/routes/index.js
@@ -1,7 +1,6 @@
 import combineRouters from 'koa-combine-routers';
 import index from './index/index.js';
 import authorization from './authorization/index.js';
-import chat from './chat/index.js';
 
 const router = combineRouters(
   index,
